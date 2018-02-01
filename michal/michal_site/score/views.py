5c583ffbfd766f88d1cb1d2c89e742d28d888d7e
ppodbielski <p.podbielski@gmail.com>
2018-02-01 21:22:31 +0100

    update

--- michal/michal_site/score/views.py
+++ michal/michal_site/score/views.py
@@ -4,4 +4,4 @@ from .models import User
 # Create your views here.
 def score_list(request):
     users = User.objects.all().order_by('score')
-    return render(request, 'score/index.html', {'users': users})
+    return render(request, 'score/main_view.xml', {'users': users})
